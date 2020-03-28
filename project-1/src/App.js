bfd01983720bf0adaeec5ffaf51792a4791d6096
CJ Roberts <cjroberts2013@gmail.com>
2020-03-27 23:45:23 -0500

    Updating app.js

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -7,24 +7,26 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      lat: null
+      lat: null,
+      errorMessage: ""
     }
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude })
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message })
+      }
     );
   }
 
   render() {
-    return (
-      <div className="App" >
-        <SeasonDisplay />
-        Latitude: {this.state.lat}
-      </div>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (something)
   }
 }
 
