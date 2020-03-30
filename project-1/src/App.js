cfbc255952706aad8b7189ee3c493cb9431809c8
CJ Roberts <cjroberts2013@gmail.com>
2020-03-29 23:34:18 -0500

    Updated app.js to display the SeasonDisplay component. Updated seasonDisplay component to calculate if it is summer or winter depending on the lat that is passed top it through props

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -4,13 +4,12 @@ import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      lat: null,
-      errorMessage: ""
-    }
+  state = {
+    lat: null,
+    errorMessage: ""
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude })
@@ -27,8 +26,10 @@ class App extends Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
+
+    return <div>Loading!</div>
   }
 }
 
