18a94e9796e36e5c7d7430b15cd688993c440b77
CJ Roberts <cjroberts2013@gmail.com>
2020-04-03 10:34:29 -0500

    Started project 3

---
+++ project-3/src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div className='search-bar ui segment'>
+                SearchBar
+            </div>
+        )
+    }
+}
+
+export default SearchBar
