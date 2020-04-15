b449501c38771135429716377e79fefa6e136f1d
CJ Roberts <cjroberts2013@gmail.com>
2020-04-14 23:14:10 -0500

    update video detail, item, and list components

--- project-3/src/components/VideoList.js
+++ project-3/src/components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from './VideoItem'
 
 const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map((video, i) => {
-        return <VideoItem key={i} onVideoSelect={onVideoSelect} video={video} />
+        return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />
     }
     );
 
